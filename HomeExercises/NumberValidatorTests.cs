d1e8d3d1c9c33e6c04d4dc8d4240c56a675a9687
g0djan <g0dj4n@gmail.com>
2017-10-23 17:31:06 +0500

    numberValidatorTests

--- HomeExercises/NumberValidatorTests.cs
+++ HomeExercises/NumberValidatorTests.cs
@@ -1,80 +1,156 @@
-﻿using System;
+using System;
 using System.Text.RegularExpressions;
 using FluentAssertions;
 using NUnit.Framework;
 
 namespace HomeExercises
 {
-	public class NumberValidatorTests
-	{
-		[Test]
-		public void Test()
-		{
-			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+    [TestFixture]
+    public class NumberValidatorTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
+            Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
 
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-			Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
-		}
-	}
+            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
+            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
+            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
+            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
+            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
+            Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
+            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
+            Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
+            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
+            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+        }
 
-	public class NumberValidator
-	{
-		private readonly Regex numberRegex;
-		private readonly bool onlyPositive;
-		private readonly int precision;
-		private readonly int scale;
 
-		public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
-		{
-			this.precision = precision;
-			this.scale = scale;
-			this.onlyPositive = onlyPositive;
-			if (precision <= 0)
-				throw new ArgumentException("precision must be a positive number");
-			if (scale < 0 || scale >= precision)
-				throw new ArgumentException("precision must be a non-negative number less or equal than precision");
-			numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
-		}
+        [Test]
+        public void NumberValidator_NegativePrecision_ThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2));
+        }
 
-		public bool IsValidNumber(string value)
-		{
-			// Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
-			// описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
-			// Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
-			// целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
-			// Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
+        [Test]
+        public void NumberValidator_ZeroPrecision_ThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new NumberValidator(0, 2));
+        }
 
-			if (string.IsNullOrEmpty(value))
-				return false;
+        [Test]
+        public void NumberValidator_NegativeScale_ThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new NumberValidator(1, -1));
+        }
 
-			var match = numberRegex.Match(value);
-			if (!match.Success)
-				return false;
+        [Test]
+        public void NumberValidator_ScaleGreaterThanPrecision_ThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new NumberValidator(1, 2));
+        }
 
-			// Знак и целая часть
-			var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
-			// Дробная часть
-			var fracPart = match.Groups[4].Value.Length;
+        [Test]
+        public void NumberValidator_ScaleEqualThanPrecision_ThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new NumberValidator(2, 2));
+        }
 
-			if (intPart + fracPart > precision || fracPart > scale)
-				return false;
+        [Test]
+        public void NumberValidator_NonNegativeScaleLessThanPositivePrecision_DoesNotThrowException()
+        {
+            Assert.DoesNotThrow(() => new NumberValidator(1));
+        }
 
-			if (onlyPositive && match.Groups[1].Value == "-")
-				return false;
-			return true;
-		}
-	}
+        [TestCase(null, ExpectedResult = false)]
+        [TestCase("", ExpectedResult = false)]
+        [TestCase("1.", ExpectedResult = false)]
+        [TestCase(".0", ExpectedResult = false)]
+        [TestCase("+.0", ExpectedResult = false)]
+        [TestCase("-.0", ExpectedResult = false)]
+        [TestCase("123-1", ExpectedResult = false)]
+        [TestCase("123+1", ExpectedResult = false)]
+        [TestCase("123-", ExpectedResult = false)]
+        [TestCase("123+", ExpectedResult = false)]
+        [TestCase("12a3", ExpectedResult = false)]
+        [TestCase("123a", ExpectedResult = false)]
+        [TestCase("a123", ExpectedResult = false)]
+        [TestCase("a", ExpectedResult = false)]
+        [TestCase("+-1", ExpectedResult = false)]
+        [TestCase("-+1", ExpectedResult = false)]
+        [TestCase("1", ExpectedResult = true)]
+        [TestCase("-1", ExpectedResult = true)]
+        [TestCase("+1", ExpectedResult = true)]
+        [TestCase("123.12", ExpectedResult = true)]
+        [TestCase("123,12", ExpectedResult = true)]
+        [TestCase("+123.12", ExpectedResult = true)]
+        [TestCase("-123.12", ExpectedResult = true)]
+        
+        public bool ValidateNumber(string number)
+        {
+            var bigPrecisionAndScale = new NumberValidator(100, 99);
+            return bigPrecisionAndScale.IsValidNumber(number);
+        }
+
+        [TestCase("12345", TestName = "Incorrect precision", ExpectedResult = false)]
+        [TestCase("1234", TestName = "Correct precision", ExpectedResult = true)]
+        [TestCase("+1234", TestName = "Incorrect precision because sign", ExpectedResult = false)]
+        [TestCase("12.34", TestName = "Correct scale", ExpectedResult = true)]
+        [TestCase("1.234", TestName = "Incorrect scale", ExpectedResult = false)]
+        [TestCase("-1", TestName = "Negative int at only-positive mode", ExpectedResult = false)]
+        [TestCase("-1.0", TestName = "Negative fraction at only-positive mode", ExpectedResult = false)]
+        public bool ValidateNumberByLength(string number)
+        {
+            return new NumberValidator(4, 2, true).IsValidNumber(number);
+        }
+    }
+
+    public class NumberValidator
+    {
+        private readonly Regex numberRegex;
+        private readonly bool onlyPositive;
+        private readonly int precision;
+        private readonly int scale;
+
+        public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
+        {
+            this.precision = precision;
+            this.scale = scale;
+            this.onlyPositive = onlyPositive;
+            if (precision <= 0)
+                throw new ArgumentException("precision must be a positive number");
+            if (scale < 0 || scale >= precision)
+                throw new ArgumentException("precision must be a non-negative number less or equal than precision");
+            numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
+        }
+
+        public bool IsValidNumber(string value)
+        {
+            // Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
+            // описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
+            // Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
+            // целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
+            // Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
+
+            if (string.IsNullOrEmpty(value))
+                return false;
+
+            var match = numberRegex.Match(value);
+            if (!match.Success)
+                return false;
+
+            // Знак и целая часть
+            var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
+            // Дробная часть
+            var fracPart = match.Groups[4].Value.Length;
+
+            if (intPart + fracPart > precision || fracPart > scale)
+                return false;
+
+            if (onlyPositive && match.Groups[1].Value == "-")
+                return false;
+            return true;
+        }
+    }
 }
